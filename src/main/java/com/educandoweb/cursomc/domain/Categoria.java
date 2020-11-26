1b263dc960eb1756ea85600e9d24a08f88a0bdfa
Jessé Carvalho <jesseccoelho@gmail.com>
2020-11-25 22:18:16 -0300

    Testando a primeira classe de domínio - Categoria

---
+++ src/main/java/com/educandoweb/cursomc/domain/Categoria.java
@@ -0,0 +1,63 @@
+package com.educandoweb.cursomc.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Categoria implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    
+    private Integer id;
+    private String nome;
+    
+    public Categoria(){
+        
+    }
+
+    public Categoria(Integer id, String nome) {
+        super();
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 79 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Categoria other = (Categoria) obj;
+        return Objects.equals(this.id, other.id);
+    }
+    
+    
+    
+}
